1ea915a1b3dfbb417d33fd3397381845be2d4c47
2ndyrGroupA <2ndyrGroupA@pn.local>
2020-11-10 15:17:06 +0800

    Updated login

--- FrontEnd/src/app/pages/login/login.page.ts
+++ FrontEnd/src/app/pages/login/login.page.ts
@@ -8,28 +8,21 @@ import { AuthService } from '../../services/auth.service';
 })
 export class LoginPage implements OnInit {
 
-  credentialsForm: FormGroup;
- 
+  credentialsForm={
+    email: "",
+    password: ""
+  }
   constructor(private formBuilder: FormBuilder, 
     private authService: AuthService) { }
  
   ngOnInit() {
-    this.credentialsForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    });
   }
  
   onSubmit() {
-    this.authService.login(this.credentialsForm.value).subscribe();
+    console.log(this.credentialsForm)
+    this.authService.login(this.credentialsForm).subscribe();
 
   }
  
-  // register() {
-  //   this.authService.register(this.credentialsForm.value).subscribe(res => {
-  //     // Call Login to automatically login the new user
-  //     this.authService.login(this.credentialsForm.value).subscribe();
-  //   });
-  // }
- 
+
 }
