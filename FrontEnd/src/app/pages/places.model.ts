9cd46b91e4a1760dec9e658ce422573407bb756b
mae-yosores2k19 <jessamaehortadoyosores@gmail.com>
2020-11-23 11:44:32 +0800

    dummyRegion

---
+++ FrontEnd/src/app/pages/places.model.ts
@@ -0,0 +1,26 @@
+export class places{
+    placeInCebu(){
+        return(
+            [
+                {val:"dal",muni:"Dalaguete"},
+                {val:"dal",muni:"Dalaguete"},
+                {val:"dal",muni:"Dalaguete"},
+                {val:"dal",muni:"Dalaguete"},
+                {val:"dal",muni:"Dalaguete"},
+                {val:"dal",muni:"Dalaguete"},
+                {val:"dal",muni:"Dalaguete"}
+            ]
+           
+
+
+        )
+    }
+}
+// <ion-item>
+//                 <ion-icon name="planet-outline"></ion-icon>
+//                 <ion-label>Region/state</ion-label>
+//             <ion-select interface="popover">
+//                 <ion-select-option *ngFor='let val of places'>{{val.muni}}</ion-select-option>
+
+//             </ion-select>
+//         </ion-item>
--- FrontEnd/src/app/pages/settings/settings.page.ts
+++ FrontEnd/src/app/pages/settings/settings.page.ts
@@ -4,6 +4,7 @@ import {PopoverController, AlertController} from '@ionic/angular';
 import {NotifSettingPage} from '../../pages/notif-setting/notif-setting.page';
 import {NavController,ModalController,MenuController} from '@ionic/angular';
 // import{ AccountsInfoPage} from '../accounts-info/accounts-info.page'
+import {places} from '../places.model';
 
 @Component({
   selector: 'app-settings',
@@ -12,11 +13,15 @@ import {NavController,ModalController,MenuController} from '@ionic/angular';
 })
 export class SettingsPage implements OnInit {
   dataFromModal;
+  public placesInCebu = new places();
+  public places = this.placesInCebu.placeInCebu()
   constructor( private popover: PopoverController, public navCtrl: NavController,
     public menuCtrl: MenuController, private modalController: ModalController,
     public alertController: AlertController)
      {this.menuCtrl.enable(true, 'main-menu'); }
   ngOnInit() {
+    console.log(this.placesInCebu.placeInCebu());
+    
   }
 
   CreatePopover(){
@@ -27,27 +32,6 @@ export class SettingsPage implements OnInit {
     })
   }
 
-  // async accInfo() {
-  //   const modal = await this.modalController.create({
-  //     component: AccountsInfoPage,
-  //     componentProps: { name: 'Jessa Mae Yosores',address:"Sa kwarto",email:"mae@gmail.com",
-  //   number:"09090909099", selfie:"gwapa.png", primaryId:"666",primaryPic:"gwapa Kaau.jpg",nbi:"nbi ni"},
-  //     cssClass: 'setting-modal',
-  //     backdropDismiss: false,
-  //   });
-
-  //   modal.present();
-  //   this.dataFromModal = await modal.onWillDismiss();
-  // }
-  async handleButtonClick() {
-      const alert = await this.alertController.create({
-       
-        // header: 'Use this lightsaber?',
-     
-        // buttons: ['Disagree', 'Agree']
-      });
-  
-      await alert.present();
-    }
+ 
 
 }
