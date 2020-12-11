3d3be778d7e727a6e613417d58a917532fce4d86
2ndyrGroupA <2ndyrGroupA@pn.local>
2020-12-11 15:45:02 +0800

    Updated History page

--- FrontEnd/src/app/pages/history/history.page.ts
+++ FrontEnd/src/app/pages/history/history.page.ts
@@ -12,4 +12,13 @@ export class HistoryPage implements OnInit {
   ngOnInit() {
   }
 
+  transactions=[
+    {type:"Cash-in", amount:"100.00", date: "05/10/1999", name: "Christopher Alonzo"},
+    {type:"Cash-in", amount:"250.00", date: "07/19/1999" , name: "Christopher Alonzo"},
+    {type:"Cash-in", amount:"200.00", date: "05/15/2000",  name: "Geneva Rivas"},
+    {type:"Cash-in", amount:"100.00", date: "05/06/1999",  name: "Geneva Rivas"},
+    {type:"Cash-out", amount:"100.00", date: "07/05/2000",  name: "Chilla Jean Cabungcag"},
+    {type:"Cash-out", amount:"350.00", date: "06/30/1999",  name: "Chilla Jean Cabungcag"},
+  ]
+
 }
