0650045fcfd40e7ba9f17b1802c1a708030b5c68
Luzmibel <luzmibel.paculanag@student.passerellesnumeriques.org>
2021-01-15 15:34:20 +0800

    Latest Version

---
+++ BackEndImageUpload/helpers/addDataTo.js
@@ -0,0 +1,33 @@
+var schema = require("../modelShcema/Schema.js");
+var express = require("express");
+var app = express();
+
+app.post("/api/imageUpload", (req, res) => {
+  const usersData = new schema(req.body);
+  try {
+    usersData.save();
+    res.send(true);
+  } catch (error) {
+    res.send(false);
+  }
+});
+app.get("/api/getAllData", async (req, res) => {
+  const usersData = await schema.find({});
+
+  try {
+    res.send(usersData);
+  } catch (err) {
+    res.send(false);
+  }
+})
+
+app.post("/api/getUserProfile", (req, res) => {
+  schema.find({name: req.body.name}, (err, result) => {
+    res.send(result)
+  })
+})
+app.get("/", (req, res) => {
+  res.send("True");
+});
+
+module.exports = app;
